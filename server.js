009ef408f1abd113d917f354e067726212cf4333
Sveta Demkovich <svetlana.demkovich@gmail.com>
2020-08-20 21:06:15 -0700

    update routes

--- server.js
+++ server.js
@@ -1,68 +1,49 @@
-const express = require("express");
-const path = require("path");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+var express = require("express");
+var path = require('path');
+var fs = require('fs');
+var bodyParser = require('body-parser')
+const data = require('./db/db.json')
 
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 3000;
 
-// const noteData = require("./db/db.json");
+// Create express app instance.
+var app = express();
+app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-// let dbJSON = noteData;
-
-
-const app = express();
-const PORT = process.env.PORT || 3000
-
-
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public"));
-
-// Static router and API connection
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
-
-
-// // POST:
-// app.post("/api/notes", function (req, res) {
-//     let newNote = req.body;
-//     dbJSON.push(newNote);
-
-//     fs.writeFile("./db/db.json", JSON.stringify(dbJSON), function (err) {
-//         if (err) throw err;
-//     });
-
-//     res.send(console.log("Successfully ADDED data to db.json"));
-// });
-
-// // DELETE: 
-// app.delete("/api/notes/:id", function (req, res) {
-//     var noteURLID = req.params.id;
-    
-//     // Loop through the notes array, when the ID is reached, remove the item at that index, re-write the edited JSON to db.json
-//     dbJSON.forEach(element => {
-//         if (noteURLID === element.id) {
-//             dbJSON.splice(dbJSON.indexOf(element), 1);
-
-//             fs.writeFile("./db/db.json", JSON.stringify(dbJSON), function (err) {
-//                 if (err) throw err;
-//             });
-//         } 
-//     });
-
-//     res.send(console.log("Successfully REMOVED data from db.json"));
-// });
-
-// Catch all, must be declared after other routes
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+app.get('/notes', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/notes.html'));
 });
 
-// app.get("/notes", function(req, res) {
-//     return res.json(reservations);
-// });
+app.get("/api/notes", function(req, res) {
+  res.send(data);
 
-// LISTENER
+});
+
+app.post("/api/notes", function(req, res) {
+  console.log(data)
+  data.push(req.body)
+  console.log(data)
+  res.send(data);
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+  console.log("id is " + req.params.id);
+  data.splice(req.params.id,1)
+  console.log(data)
+  res.send(data);
+});
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
   });
